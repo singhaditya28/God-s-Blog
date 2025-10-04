9f77049dbbf664c687cdaba240120e4185f2d94f
Aditya Singh <singhaditya28@hotmail.com>
2025-10-04 23:41:42 +0530

    Fix itsdangerous version for compatibility
    - Downgraded itsdangerous to 2.0.1 for TimedJSONWebSignatureSerializer support
    - This should resolve the import error in models.py

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import URLSafeTimedSerializer
 from flask import current_app
 from flaskblog import db, login_manager
 from flask_login import UserMixin
@@ -19,14 +19,14 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
-        return s.dumps({'user_id': self.id}).decode('utf-8')
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id})
 
     @staticmethod
-    def verify_reset_token(token):
-        s = Serializer(current_app.config['SECRET_KEY'])
+    def verify_reset_token(token, expires_sec=1800):
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
         try:
-            user_id = s.loads(token)['user_id']
+            user_id = s.loads(token, max_age=expires_sec)['user_id']
         except:
             return None
         return User.query.get(user_id)
