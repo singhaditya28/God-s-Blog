7b5ef2f671c95b870b951db5812288bb9641ea3b
Aditya Singh <singhaditya28@hotmail.com>
2025-10-04 23:21:06 +0530

    Fix Render deployment with Python 3.11 and stable package versions
    - Added runtime.txt to specify Python 3.11.6 for better compatibility
    - Downgraded to stable package versions that work with Python 3.11
    - Reverted to TimedJSONWebSignatureSerializer for itsdangerous 2.1.2
    - Used Pillow 9.5.0 which has better wheel support
    - Fixed Flask-SQLAlchemy to 3.0.5 for stability

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from itsdangerous import URLSafeTimedSerializer as Serializer
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flaskblog import db, login_manager
 from flask_login import UserMixin
@@ -19,14 +19,14 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(current_app.config['SECRET_KEY'])
-        return s.dumps({'user_id': self.id}, salt='password-reset')
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
 
     @staticmethod
-    def verify_reset_token(token, expires_sec=1800):
+    def verify_reset_token(token):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
-            user_id = s.loads(token, salt='password-reset', max_age=expires_sec)['user_id']
+            user_id = s.loads(token)['user_id']
         except:
             return None
         return User.query.get(user_id)
